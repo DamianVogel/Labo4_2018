4d33bdde25a7869e1deec2ba77715702a416f8b0
Damian Vogel <rodolfovogel@gmail.com>
2018-09-01 16:29:32 -0300

    Probando
    Se prueban componentes.

--- Pruebas Angular/src/app/app.module.ts
+++ Pruebas Angular/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PruebaComponenteComponent } from './componentes/prueba-componente/prueba-componente.component';
+
+const miRuteo = [{path:'prueba', component: PruebaComponenteComponent}
+
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponenteComponent
+    
   ],
   imports: [
     BrowserModule
